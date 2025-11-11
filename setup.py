ce0be6798ebfd8bb0e9c586fe1019e3f56be1e9c
jason <jasonah@seas.upenn.edu>
2025-11-11 17:28:30 -0500

    initial ros2 commit

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
-d = generate_distutils_setup(
-    packages=['rostak'],
-    scripts=['scripts/rostak_bridge', 'scripts/roscot_fix'],
-    package_dir={'': 'src'}
+package_name = 'rostak'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jason',
+    maintainer_email='jason@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            '',
+        ],
+    },
 )
-
-setup(**d)
--- src/rostak/roscot_fix.py
+++ src/rostak/roscot_fix.py
@@ -1,3 +1,4 @@
+"""
 import rospy
 from rostak.cot_utility import CotUtility
 from std_msgs.msg import String
@@ -24,3 +25,33 @@ class RosCotFix:
 if __name__ == '__main__':
     RosCotFix()
     rospy.spin()
+"""
+import rclpy
+from rclpy.node import Node
+from rostak.cot_utility import CotUtility
+from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+
+class RosCotFix(Node):
+
+    def __init__(self) -> None:
+        self.declare_parameter("cot_params","./")
+        config_path = self.get_parameter("cot_params").value
+        self.util_ = CotUtility(config_path)
+        
+        self.declare_parameter("rate", 0.2)
+        self.rate_ = self.get_parameter("rate").value
+
+        self.msg_ = String()
+
+        self.pub_ = self.create_publisher(String, "tak_tx", 1)
+
+        self.create_subscription(NavSatFix, "fix", self.fix_callback)
+        self.get_logger().info(self.util_.get_config())
+
+    def fix_callback(msg : NavSatFix) -> None:
+        """Generate a status COT event"""
+        self.util_.set_point(msg)
+        stale_in = 2 * max(1, 1 / self.rate_)
+        self.msg_.data = self.util_.new_status_msg(stale_in)
+        self.pub_.publish(self.msg)
